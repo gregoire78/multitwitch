3283776d1e2689369d777a56b09d5b10cddd4079
ITELIOS\gjoncour <gregoire.joncour@itelios.com>
2019-02-27 17:42:51 +0100

    add login to twitch for streams followed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import _ from "lodash";
+import NewWindow from 'react-new-window';
+import axios from 'axios';
 import { WidthProvider, Responsive } from "react-grid-layout";
 //import Twitch from './Twitch';
 import MyIcon from './Combo_Purple_RGB.svg'
@@ -8,7 +10,8 @@ import { CSSTransition } from 'react-transition-group';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTimes, faEdit, faLayerGroup, faPlus, faAngleDoubleRight, faAngleDoubleLeft } from '@fortawesome/free-solid-svg-icons'
+import { faTimes, faEdit, faLayerGroup, faPlus, faAngleDoubleRight, faAngleDoubleLeft, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import { faTwitch } from '@fortawesome/free-brands-svg-icons';
 
 import '../node_modules/react-resizable/css/styles.css';
 import '../node_modules/react-grid-layout/css/styles.css'
@@ -16,7 +19,7 @@ import './App.css';
 import GridTwitch from './GridTwitch';
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 const originalLayouts = getFromLS("layouts") || {};
-library.add(faTimes, faEdit, faLayerGroup, faPlus, faAngleDoubleRight, faAngleDoubleLeft);
+library.add(faTimes, faEdit, faLayerGroup, faPlus, faAngleDoubleRight, faAngleDoubleLeft, faTwitch, faSignOutAlt);
 
 class App extends Component {
   static defaultProps = {
@@ -42,7 +45,10 @@ class App extends Component {
       showOverlay: false,
       isEditMode: true,
       mounted: false,
-      isCollapse: false
+      isCollapse: false,
+      opened: false,
+      isAuth: localStorage.getItem('token'),
+      streams: []
     };
 
     this.onLayoutChange = this.onLayoutChange.bind(this);
@@ -57,9 +63,20 @@ class App extends Component {
     this.handleEdit = this.handleEdit.bind(this);
     this.onToogleCollapse = this.onToogleCollapse.bind(this);
     this.onRemoveItem = this.onRemoveItem.bind(this);
+    this.handleWindow = this.handleWindow.bind(this);
+    this.logout = this.logout.bind(this);
   }
 
   componentDidMount() {
+    if(window.location.hash) {
+      let search = window.location.hash.substring(1);
+      search = JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
+      localStorage.setItem('token', search.access_token)
+      window.close();
+    }
+    if(this.state.isAuth) {
+      this.getFollowedStream();
+    }
     document.body.style.backgroundImage = "url("+MyIcon+")";
     document.body.style.backgroundPosition = "center";
     document.body.style.backgroundRepeat = "no-repeat";
@@ -195,34 +212,111 @@ class App extends Component {
     }));
   }
 
+  /*openPopup() {
+    const width = 600, height = 600
+    const left = (window.innerWidth / 2) - (width / 2)
+    const top = (window.innerHeight / 2) - (height / 2)
+    const url = `https://id.twitch.tv/oauth2/authorize?client_id=wkyn43dnz5yumupaqv8vwkz1j4thi1&redirect_uri=http://localhost:3000/&response_type=token&scope=user_read`
+
+    return window.open(url, '',
+      `toolbar=no, location=no, directories=no, status=no, menubar=no, 
+      scrollbars=no, resizable=no, copyhistory=no, width=${width}, 
+      height=${height}, top=${top}, left=${left}`
+    )
+  }*/
+
+  handleWindow() {
+    this.setState(prevState => ({
+      opened: !prevState.opened
+    }));
+  }
+
+  logout() {
+    this.setState({isAuth: false, streams: []});
+    localStorage.removeItem('token');
+  }
+
+  handleClosePopup() {
+    this.setState({opened: false, isAuth: localStorage.getItem('token') && true});
+    this.getFollowedStream();
+  }
+
+  getFollowedStream() {
+    axios.get(`https://api.twitch.tv/kraken/streams/followed`, { 
+      headers: { 
+        'Authorization': `OAuth ${localStorage.getItem('token')}`,
+        'Client-ID': 'wkyn43dnz5yumupaqv8vwkz1j4thi1' 
+      } 
+    } ).then(res => {
+        const streams = res.data.streams;
+        this.setState({ streams });
+      })
+  }
+
+  addFollow(name){
+    const pseudos = this.state.pseudos;
+    if(!_.includes(this.state.pseudos, name)) {
+      window.history.replaceState('','',`${window.location}${window.location.href.slice(-1) === '/' ? '' : '/'}${name}`);
+      pseudos.push(name);
+      let layout = this.generateLayout(pseudos)
+      this.setState({pseudos, layout});
+    }
+  }
+
   render() {
+    const { opened, isEditMode, input, pseudos, isCollapse, showOverlay, layout, layouts, isAuth, streams } = this.state
     return (
-      <div>
+      <>
+        { opened &&
+          <NewWindow
+            onUnload={this.handleClosePopup.bind(this)}
+            url={`https://id.twitch.tv/oauth2/authorize?client_id=wkyn43dnz5yumupaqv8vwkz1j4thi1&redirect_uri=http://localhost:3000/&response_type=token&scope=user_read`}
+            features={ { left: (window.innerWidth / 2) - (600 / 2), top: (window.innerHeight / 2) - (600 / 2), width: 600, height: 600 } }
+          >
+            <h5>Here is a textbox. Type something in it and see it mirror to the parent.</h5>
+          </NewWindow>
+        }
+
         <CSSTransition
           in={this.state.isCollapse}
           classNames="header"
           timeout={300}
         >
           <header>
-            <button onClick={this.resetLayout}><FontAwesomeIcon icon="layer-group" /></button>
-            <button onClick={this.handleEdit}><FontAwesomeIcon icon="edit" color={this.state.isEditMode ? "black" : "grey"} /></button>
+            <nav>
+              <button onClick={this.resetLayout}><FontAwesomeIcon icon="layer-group" /></button>
+              <button onClick={this.handleEdit}><FontAwesomeIcon icon="edit" color={isEditMode ? "black" : "grey"} /></button>
 
-            <form onSubmit={this.addPseudo}>
-              <input type="text" value={this.state.input} onChange={ this.handleChange } placeholder="channel twitch"/>
-              <button type="submit" disabled={this.state.input.length <= 0 || this.state.pseudos.find((v,k) => v === this.state.input)}><FontAwesomeIcon icon="plus" /></button>
-            </form>
+              <form onSubmit={this.addPseudo}>
+                <input type="text" value={input} onChange={ this.handleChange } placeholder="channel twitch"/>
+                <button type="submit" disabled={input.length <= 0 || pseudos.find((v,k) => v === input)}><FontAwesomeIcon icon="plus" /></button>
+              </form>
 
-            <button onClick={this.onToogleCollapse}><FontAwesomeIcon icon={this.state.isCollapse ? "angle-double-right" : "angle-double-left"} /></button>
+              {!isAuth ? 
+              <button onClick={this.handleWindow} title="Login to twitch account"><FontAwesomeIcon icon={["fab","twitch"]} color="#6441A4" /></button> : 
+              <button onClick={this.logout} title="Logout"><FontAwesomeIcon icon="sign-out-alt" color="#6441A4" /></button>
+              }
+              <button onClick={this.onToogleCollapse}><FontAwesomeIcon icon={isCollapse ? "angle-double-right" : "angle-double-left"} /></button>
+              
+            </nav>
+            {!_.isEmpty(streams) && isEditMode && <nav className="streams">
+                <p style={{textAlign: "center", background: "#5a3a93"}}>Streams on air</p>
+                {_.map(streams, (v,k) => {
+                  return (
+                    <p key={k} onClick={this.addFollow.bind(this, v.channel.name)}><span role="img" aria-label="on air">🔴</span>{v.channel.display_name}</p>
+                  )
+                })}
+            </nav>}
           </header>
         </CSSTransition>
 
-        {this.state.pseudos.length > 0 ?
+        {pseudos.length > 0 ?
         <ResponsiveReactGridLayout
           margin={[10,10]}
           containerPadding={[10,10]}
           onLayoutChange={this.onLayoutChange}
           onResize={this.onResize}
-          layouts={this.state.layouts}
+          layouts={layouts}
           onBreakpointChange={this.onBreakpointChange}
           onResizeStart={this.showOverlay}
           onResizeStop={this.hideOverlay}
@@ -231,16 +325,16 @@ class App extends Component {
           measureBeforeMount={true}
           {...this.props}
         >
-          {_.map(this.state.layout, (l,k) => {
+          {_.map(layout, (l,k) => {
             return (
-              <div key={l.i} data-grid={l} style={this.state.isEditMode?{padding:'5px', outline: '5px dashed #5a3a93', outlineOffset: '-5px'}:''}>
-                <GridTwitch isEditMode={this.state.isEditMode} showOverlay={this.state.showOverlay} l={l} onRemoveItem={this.onRemoveItem} />
+              <div key={l.i} data-grid={l} style={isEditMode && {padding:'5px', outline: '5px dashed #5a3a93', outlineOffset: '-5px'}}>
+                <GridTwitch isEditMode={isEditMode} showOverlay={showOverlay} l={l} onRemoveItem={this.onRemoveItem} />
               </div>
             )
           })
           }
         </ResponsiveReactGridLayout> : ''}
-      </div>
+      </>
     );
   }
 }
