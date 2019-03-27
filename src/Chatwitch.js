7cc9e651bf5bf9df2736a2665133787bfeff5ebf
ITELIOS\gjoncour <gregoire.joncour@itelios.com>
2019-03-27 17:54:28 +0100

    clean setup

--- src/Chatwitch.js
+++ src/Chatwitch.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import tmi from 'tmi.js';
 import axios from 'axios';
 import _ from 'lodash';
-
+import { HotKeys } from "react-hotkeys";
+import moment from 'moment';
+import 'moment/locale/fr';
+moment.locale('fr');
 export default class Chatwitch extends Component {
     constructor(props) {
         super(props);
@@ -24,6 +27,7 @@ export default class Chatwitch extends Component {
             ],
             channelsDetails : [],
             chatThreads: [],
+            autoscroll: true
         }
         this.badgesGlobal = {};
         this.client = new tmi.client({
@@ -53,6 +57,8 @@ export default class Chatwitch extends Component {
 
         this.badgesGlobal = (await axios.get(`https://badges.twitch.tv/v1/badges/global/display?language=fr`)).data
         let dd = {}
+        this.infoChannels = await this.getInfoChannels();
+        console.table(this.infoChannels)
         _.map(this.state.channels, async (channel)=> {
             dd[channel]=await this.getBadgeLink(channel)
         })
@@ -61,8 +67,21 @@ export default class Chatwitch extends Component {
     }
 
     componentDidMount() {
+        let lastScrollTop = 0
+        window.addEventListener('scroll',(e)=>{
+            let st = window.pageYOffset || document.documentElement.scrollTop;
+            if (st > lastScrollTop){
+                // downscroll code
+            } else {
+                this.setState({autoscroll: false});
+            }
+            lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+            if (window.pageYOffset === document.body.scrollHeight - window.innerHeight && !this.state.autoscroll) {
+                this.setState({autoscroll: true});
+            }
+        }, true);
         this.client.on("chat", (channel, user, message, self)=>{
-            let chat = {status: "message", message, channel:{name:channel, display_name: this.state.channelsDetails[channel.slice(1)].display_name}, badgesUser:[], user};
+            let chat = {status: "message", message, channel:{name:channel, display_name: this.state.channelsDetails[channel.slice(1)].display_name}, badgesUser:[], user, ts: moment(user["tmi-sent-ts"], "x").format('LT')};
             if(user.badges) {
                 chat.badgesUser = _.map(user.badges, (v,k)=>{return this.state.channelsDetails[channel.slice(1)].badges[k].versions[v]})
             }
@@ -94,74 +113,57 @@ export default class Chatwitch extends Component {
         console.log(this.state.connecting ? "Connecting to : "+address+":"+port : "Connected to : "+address+":"+port);
     }
 
+    async getInfoChannels() {
+        return (await axios.get(`https://api.twitch.tv/helix/users?login=${this.state.channels.join('&login=')}`, {
+            headers: {
+                'Client-ID': process.env.REACT_APP_TWITCH_CLIENTID
+            }
+        })).data.data;
+    }
+
     async getBadgeLink(channel) {
-        const infoChannel = (await axios.get(`https://api.twitch.tv/helix/users?login=${channel}`, {
+        const infoChannel = _.find(this.infoChannels, user => user.login === channel);
+        console.table(infoChannel)
+        const infoStream = (await axios.get(`https://api.twitch.tv/helix/streams?user_login=${channel}`, {
             headers: {
                 'Client-ID': process.env.REACT_APP_TWITCH_CLIENTID
             }
         })).data;
-        const badgesChannel = (await axios.get(`https://badges.twitch.tv/v1/badges/channels/${infoChannel.data[0].id}/display?language=fr`)).data;
-        return {id: infoChannel.data[0].id, display_name: infoChannel.data[0].display_name, badges:{...this.badgesGlobal.badge_sets, ...badgesChannel.badge_sets}}
+        const badgesChannel = (await axios.get(`https://badges.twitch.tv/v1/badges/channels/${infoChannel.id}/display?language=fr`)).data;
+        return {id: infoChannel.id, display_name: infoChannel.display_name, badges:{...this.badgesGlobal.badge_sets, ...badgesChannel.badge_sets}, infoChannel: infoChannel, infoStream: infoStream.data[0]}
     }
 
     scrollToBottom = () => {
-        this.messagesEnd.current.scrollIntoView({ behavior: 'smooth' })
+        if(this.state.autoscroll)
+            this.messagesEnd.current.scrollIntoView({ behavior: 'smooth' })
     }
-
-    /*parseFormatEmotes(text, emotes, key) {
-        var splitText = text.split('');
-        for(var i in emotes) {
-            var e = emotes[i];
-            for(var j in e) {
-                var mote = e[j];
-                if(typeof mote == 'string') {
-                    mote = mote.split('-');
-                    mote = [parseInt(mote[0]), parseInt(mote[1])];
-                    var length =  mote[1] - mote[0],
-                        empty = Array.apply(null, new Array(length + 1)).map(function() { return '' });
-                    splitText = splitText.slice(0, mote[0]).concat(empty).concat(splitText.slice(mote[1] + 1, splitText.length));
-                    splitText.splice(mote[0], 1, <img key={key-mote[0]} alt="" style={{verticalAlign: "text-top"}} className="emoticon" title={text.slice(mote[0],mote[1]+1)} src={`http://static-cdn.jtvnw.net/emoticons/v1/${i}/1.0`} />);
-                }
-            }
-        }
-        return splitText;
-    }*/
-
-    /*altr(message, emotes, key) {
-        console.log(emotes)
-        for(var i in emotes) {
-            var e = emotes[i];
-            for(var j in e) {
-                var mote = e[j];
-                mote = mote.split('-');
-                mote = [parseInt(mote[0]), parseInt(mote[1])];
-                var length = mote[1] - mote[0];
-                var re = new RegExp(`(.{${mote[0]}}).{${length+1}}`);
-                message = message.replace(re, `http://static-cdn.jtvnw.net/emoticons/v1/${i}/1.0`);
-            }
-        }
-        return message;
-    }*/
 
     render() {
         return (
-            <>
-                <p>{this.state.connecting && "connecting to chat irc"}</p>
-                <div style={{minHeight: "100%", background: "black", color: "white"}}>{this.state.chatThreads.map((chatThread,k)=>{
+            <HotKeys keyMap={{
+                ALT: { sequence: "alt" }
+              }} handlers={{
+                ALT: event => {this.setState(prevState=>({autoscroll: !prevState.autoscroll}))},
+            }}>
+                <div style={{position: "fixed", right: 0, fontSize: 12}}>{_.map(this.state.channelsDetails, (channelDetails,k)=>{return(<span key={k} style={{background: "#"+intToRGB(hashCode("#"+k)), color: "white"}}>{channelDetails.display_name}</span>)})}</div>
+                {this.state.connecting && <p>connecting to chat irc</p>}
+                <div>
+                {this.state.chatThreads.map((chatThread,k)=>{
                     let thread;
                     if(chatThread.status === "to"){
-                        thread = (<>@{chatThread.username} you are timed out for {chatThread.duration} seconds. </>)
+                        thread = (<b style={{background: "yellow", color: "black"}}>@{chatThread.username} you are timed out for {chatThread.duration} seconds.</b>)
                     }
                     if(chatThread.status === "ban"){
-                        thread = (<>@{chatThread.username} you are BANNED. </>)
+                        thread = (<b style={{background: "red"}}>@{chatThread.username} you are BANNED.</b>)
                     }
                     if(chatThread.status === "message"){
-                        thread = (<>{chatThread.badgesUser.map((badgeUser, k)=>{return <img key={k} src={badgeUser.image_url_1x} alt="" title={badgeUser.title} style={{verticalAlign:"text-bottom"}} />})} <span style={{color: chatThread.user.color, fontWeight: "bold"}}>{chatThread.user["display-name"]}</span> : <span dangerouslySetInnerHTML={{ __html: formatEmotes(chatThread.message, chatThread.user.emotes, k) }} /> </>)
+                        thread = (<><small style={{color: "grey"}}>{chatThread.ts}</small> {chatThread.badgesUser.map((badgeUser, k)=>{return <img key={k} src={badgeUser.image_url_1x} alt="" title={badgeUser.title} style={{verticalAlign:"text-bottom"}} />})} <span style={{color: chatThread.user.color, fontWeight: "bold"}}>{chatThread.user["display-name"]}</span> : <span dangerouslySetInnerHTML={{ __html: formatEmotes(chatThread.message, chatThread.user.emotes, k).replace(/(?:^|\s)((?:http|https|ftp|ftps):\/\/[a-zA-Z0-9\-.]+\.[a-zA-Z]{2,3}(\/\S*)?)/g, " <a href=$1 target='_blank'>$1</a>") }} /> </>)
                     }
-                    return (<div key={k} style={{minHeight: "28px"}}><span style={{background: "#"+intToRGB(hashCode(chatThread.channel.name)), color: "white"}}>{chatThread.channel.display_name}</span> {thread} </div>);
-                })}</div>
+                    return (<div key={k} style={{minHeight: "28px"}}><img title={chatThread.channel.display_name} style={{height: 22, verticalAlign: "text-top", border: `3px solid ${"#"+intToRGB(hashCode(chatThread.channel.name))}`, background: "#"+intToRGB(hashCode(chatThread.channel.name))}} src={this.state.channelsDetails[chatThread.channel.name.slice(1)].infoChannel.profile_image_url} alt="" /> {thread} </div>);
+                })}
+                </div>
                 <div ref={this.messagesEnd} />
-            </>
+            </HotKeys>
         )
     }
 }
@@ -195,7 +197,9 @@ function formatEmotes(text, emotes) {
                 var length =  mote[1] - mote[0],
                     empty = Array.apply(null, new Array(length + 1)).map(function() { return '' });
                 splitText = splitText.slice(0, mote[0]).concat(empty).concat(splitText.slice(mote[1] + 1, splitText.length));
-                splitText.splice(mote[0], 1, `<img style="vertical-align: text-top;" class="emoticon" title=${text.slice(mote[0],mote[1]+1)} src="http://static-cdn.jtvnw.net/emoticons/v1/${i}/1.0">`);
+                splitText.splice(mote[0], 1, `<img style="vertical-align: text-top;" class="emoticon" title=${text.slice(mote[0],mote[1]+1).replace(/[\u00A0-\u9999<>&]/gim, function(i) {
+                    return '&#'+i.charCodeAt(0)+';';
+                 })} src="http://static-cdn.jtvnw.net/emoticons/v1/${i}/1.0">`);
             }
         }
     }
