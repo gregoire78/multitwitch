9b74c3c40354b39c389247925531ce9a73a91754
Grégoire Joncour <greg.joncour@gmail.com>
2019-09-26 22:05:00 +0200

    nieme modif

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ class App extends Component {
     person.layout = this.generateLayout(urlparse);
     person.pseudos = urlparse;
     person.isAuth = cookies.get('token') && cookies.get('token').length > 0;
-    person.isResetMode = getFromLS('isResetMode') !== undefined ? getFromLS('isResetMode') : true;
+    person.isResetMode = getFromLS('isResetMode') !== undefined ? getFromLS('isResetMode') : false;
     this.onLayoutChange = this.onLayoutChange.bind(this);
     this.resetLayout = person.resetLayout.bind(person, saveToLS);
     this.addPseudo = this.addPseudo.bind(this);
