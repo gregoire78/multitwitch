b140d033b2fd9cfade3b034d0bb8a2d271a83f4e
gregoire78 <gregoire@joncour.dev>
2022-10-22 22:15:02 +0200

    counter dev

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React, { lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CookiesProvider } from "react-cookie";
-import { MatomoProvider, createInstance } from "@datapunt/matomo-tracker-react";
 import process from "process";
 import "@fontsource/ubuntu";
 import "./index.css";
@@ -10,41 +9,43 @@ import "./i18n";
 const App = lazy(() => import("./components/App"));
 const RedirPage = lazy(() => import("./components/Redirect"));
 import * as serviceWorker from "./serviceWorker";
-
-const instance = createInstance({
-  urlBase: process.env.MATOMO_URL,
-  siteId: process.env.MATOMO_ID,
-  disabled: process.env.NODE_ENV !== "production",
-});
+import { Helmet } from "react-helmet";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <CookiesProvider>
-    <MatomoProvider value={instance}>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            exact
-            path="/redirect"
-            element={
-              <Suspense fallback="">
-                <RedirPage />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/*"
-            element={
-              <Suspense fallback="">
-                <App />
-              </Suspense>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </MatomoProvider>
+    <Helmet>
+      {process.env.NODE_ENV !== "development" && (
+        <script
+          src={process.env.COUNTER_URL}
+          data-id={process.env.COUNTER_ID}
+          data-utcoffset={process.env.COUNTER_UTCOFFSET}
+        ></script>
+      )}
+    </Helmet>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          exact
+          path="/redirect"
+          element={
+            <Suspense fallback="">
+              <RedirPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/*"
+          element={
+            <Suspense fallback="">
+              <App />
+            </Suspense>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   </CookiesProvider>
 );
 
